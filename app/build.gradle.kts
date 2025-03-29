b3f62fcf5b26c78e9e9f55f10fece89dee31c733
TakWolf <takwolf@foxmail.com>
2025-03-30 05:01:52 +0800

    升级 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.demo.lock9view"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.takwolf.android.demo.lock9view"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.0.1"
     }
--- lock9view/build.gradle.kts
+++ lock9view/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.takwolf.android.lock9view"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21
